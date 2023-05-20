68bd616a7887a110fe2c22799b64a71e9ab3326d
Sai Rajath <48343927+TheRajath@users.noreply.github.com>
2023-05-20 15:41:29 +0530

    Seed mongodb (#2)
    * create gadgets model
    
    * setup cloudinary
    
    * Create seeds data

---
+++ models/gadget.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function () {
+
+    return this.url.replace('/upload', '/upload/w_200');
+});
+
+const opts = { toJSON: { virtuals: true } };
+
+const GadgetSchema = new Schema({
+    title: String,
+    category: String,
+    brand: String,
+    price: Number,
+    description: String,
+    inStock: Boolean,    
+    images: [ImageSchema]
+}, opts);
+
+module.exports = mongoose.model('Gadget', GadgetSchema);
---
+++ seeds/gadgetList.js
@@ -0,0 +1,25 @@
+module.exports = [
+    {
+        gadgetId: 'Apple1401',
+        title: 'Apple iPhone 14 Pro (128 GB) - Deep Purple',
+        category: 'Mobile',
+        brand: 'Apple',
+        price: 119999,
+        inStock: true,
+        description: "A magical way to interact with your iPhone. A vital safety feature designed to save lives. \
+        An innovative 48MP Main camera. And a display that's up to 2x brighter in the sun. \
+        All powered by the ultimate smartphone chip."
+    },
+    {
+        gadgetId: 'SamsungS2301',
+        title: 'Samsung Galaxy S23 Plus 5G (Phantom Black, 8GB, 256GB Storage)',
+        category: 'Mobile',
+        brand: 'Samsung',
+        price: 94999,
+        inStock: true,
+        description: "Get ready for a Gallery full of epic night shots everyone will \
+        want. Nightographyls enhanced Al keeps details clear, so low \
+        light photos and videos will be bright and colorful \
+        from dusk to dawn and back again."
+    }
+];
