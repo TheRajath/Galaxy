6a066e33b15aaf2e0b5c78618ccf9c8a9665771e
Sai Rajath <sairajath7@gmail.com>
2023-05-21 21:05:44 +0530

    show all gadgets

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const path = require('path');
 const ejsMate = require('ejs-mate');
 const express = require('express');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const Gadget = require('./models/gadget');
 
 mongoose.connect(process.env.DATA_SOURCE_LOCAL);
 
@@ -21,11 +23,20 @@ const app = express();
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
 
     res.render('home');
+});
+
+app.get('/gadgets', async (req, res) => {
+
+    const gadgets = await Gadget.find({});
+    res.render('gadgets/index', { gadgets });
 });
 
 app.listen(3000, () => {
